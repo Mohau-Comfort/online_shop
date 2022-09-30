6f27eca3a597044845834e4613b056fd7d48e2cc
Mohau-Comfort <69164161+Mohau-Comfort@users.noreply.github.com>
2022-09-30 12:37:22 +0200

    added products page and functionality

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import {AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai';
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='footer-container'>
+      <p>Copyright © {new Date().getFullYear()} Mohau's Audio & Speakers All rights reserved</p>
+      <p className='icons'>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
     </div>
   )
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import {AiOutlineShopping} from 'react-icons/ai';
 
+import { urlFor } from '../lib/client';
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href="/">
+          Mohau's Audio and Speakers
+        </Link>
+      </p>
+      <button type='button' className='cart-icon' onClick="">
+        <AiOutlineShopping /> <span className='cart-item-qty'>1</span>
+      </button>
     </div>
   )
 }
