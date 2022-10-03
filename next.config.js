47cb0cd1748e55fa87969998a206d87ecc3a09ef
Mohau-Comfort <69164161+Mohau-Comfort@users.noreply.github.com>
2022-10-03 13:41:52 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,4 +4,10 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+const  eslint = {
+  // Warning: This allows production builds to successfully complete even if
+  // your project has ESLint errors.
+  ignoreDuringBuilds: true,
+}
+
+module.exports = nextConfig, eslint
